917a47c0ff3e4e7563b7b1c87d85b6b53d07ca82
Juan E. Sanchez <juan@tcad.com>
2023-12-24 00:35:53 -0600

    json file to read is command line argument

--- pyadms/nstart.py
+++ pyadms/nstart.py
@@ -1,7 +1,8 @@
 import pyadms.adms_loader
 import pyadms.adms_implicit as adms_implicit
+import sys
 
-admst = pyadms.adms_loader.load_json('foo.json')
+admst = pyadms.adms_loader.load_json(sys.argv[1])
 dv = adms_implicit.dependency_visitor()
 admst.get_module().visit(dv)
 

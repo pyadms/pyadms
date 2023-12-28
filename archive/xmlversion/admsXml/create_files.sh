b3b9b4658c512dea61376144461ac2220ee31b10
Juan E. Sanchez <juan@tcad.com>
2023-12-27 22:14:40 -0600

    remove xmlversion

--- archive/xmlversion/admsXml/create_files.sh
+++
@@ -1,8 +0,0 @@
-/usr/bin/perl ./mkelements.pl ..
-
-for i in constants.vams disciplines.vams adms.implicit.xml dummy.va dummy.xml; do
-/usr/bin/perl ./mkctext.pl $i ..
-done
-
-/usr/bin/perl ./mkgrammar.pl ./verilogaYacc.y.in
-

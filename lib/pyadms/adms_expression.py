d1410cd7850cb0f40a27d676677bdea48f61fc95
Juan E. Sanchez <juan@tcad.com>
2024-01-20 15:06:20 -0600

    start some derivative calculations

--- lib/pyadms/adms_expression.py
+++ lib/pyadms/adms_expression.py
@@ -17,13 +17,37 @@ class add(expression_base):
         self.args = kwargs['args']
 
     def get_expression(self):
-        t = [a.get_expression() for a in self.args]
+        e = [a.get_expression() for a in self.args]
         return ' + '.join(t)
 
     def get_derivative(self, arg):
         # zero elimination later
-        t = [a.get_derivative(arg).expression() for a in self.args]
+        d = [a.get_derivative(arg).expression() for a in self.args]
         return ' + '.join(t)
 
 
+class multiply(expression_base):
+    __slots__ = ('args')
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.args = kwargs['args']
+
+    def get_expression(self):
+        t = [a.get_expression() for a in self.args]
+        return ' * '.join(t)
+
+    def get_derivative(self, arg):
+        # zero elimination later
+        # one multiple elimination
+        # (a*b*c)' = a'*(b*c) + b'*(a*c)+c'*(a*b)
+        e = [a.get_expression() for a in self.args]
+        d = [a.get_derivative(arg).expression() for a in self.args]
+        o = []
+
+        for i, ld in enumerate(d):
+            le =e[:]
+            le[i] = ld
+            o.append(' * '.join(le))
+        return ' * '.join(o)
 

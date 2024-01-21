8a32673bac2ed508cdef37e7c690deeba5fbee3b
Juan E. Sanchez <juan@tcad.com>
2024-01-21 00:15:42 -0600

    work on fixing up modules

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
-from setuptools import setup, Distribution
+from setuptools import setup, dist
 import os
 import re
 
 #https://stackoverflow.com/questions/35112511/pip-setup-py-bdist-wheel-no-longer-builds-forced-non-pure-wheels/36886459#36886459
 #https://stackoverflow.com/questions/24071491/how-can-i-make-a-python-wheel-from-an-existing-native-library/24793171#24793171
 #https://github.com/google/or-tools/issues/616
-class BinaryDistribution(Distribution):
+class BinaryDistribution(dist.Distribution):
     """Distribution which always forces a binary package with platform name"""
-    def has_ext_modules(_):
+    def has_ext_modules(self):
         return True
     def is_pure(self):
+        raise RuntimeError("HERE")
         return False
 
 setup(
@@ -21,10 +22,11 @@ setup(
             ],
         },
         package_dir = {
-            '' : 'pyadms',
+            '' : 'lib',
         },
-        #package_data={
-        #    'pyadms' : ['./admsJSON',],
-        #},
+        package_data={
+            'pyadms' : ['./admsJSON',],
+        },
         distclass=BinaryDistribution,
+        #include_package_data=True,
     )

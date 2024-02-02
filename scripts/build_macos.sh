1e177fc2fc122dc573db715c5fb55f6bc454c9e3
Juan E. Sanchez <juan@tcad.com>
2024-02-02 17:35:28 -0600

    script for macos to build universal wheel

--- scripts/build_macos.sh
+++ scripts/build_macos.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 set -e
 set -u
+mkdir -p build_macos
+# automatically copied to lib directory
+(cd build_macos && cmake -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" .. && make -j4)
+# need to create pip package for each platform
+python3 -mvenv venv
+source venv/bin/activate
+pip install wheel
+pip wheel .
